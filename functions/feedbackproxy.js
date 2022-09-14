3494ba8ca31c65e0f797328cc1431f454380b626
lk <lasse.klein@gmail.com>
2022-09-14 21:35:02 +0200

    Cleaned up

--- functions/feedbackproxy.js
+++ functions/feedbackproxy.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
-const qs = require('querystring');
 const Airtable = require('airtable')
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
     Airtable.configure({apiKey: process.env.AIRTABLE_API_KEY})
     const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
     const baseName = 'FeedbackForm'
-    if (event['httpMethod'] == 'POST') {
+    if (event['httpMethod'] === 'POST') {
         let post = JSON.parse(event.body)
         const data = [{'fields': post}]
 

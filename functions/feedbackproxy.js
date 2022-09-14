e4b746cbdef5f11a773f2d30f9d8493e13ae0bd0
lk <lasse.klein@gmail.com>
2022-09-14 19:42:54 +0200

    Start

--- functions/feedbackproxy.js
+++ functions/feedbackproxy.js
@@ -10,15 +10,8 @@ exports.handler = async (event, context) => {
         let post = JSON.parse(event.body)
         const data = [{'fields': post}]
 
-        await base(baseName).create(data,
-            {typecast: true},
-            function (err,records) {
-                if (err) {console.error(err); return; }
-                records.forEach(function (record) {
-                   // console.log(record.getId())
-                })
-            }
-        )
+        await base(baseName).create(data, {typecast: true} )
+
         return {
             statusCode: 200,
             body: 'Success!',

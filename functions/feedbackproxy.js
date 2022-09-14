0c4d62307075462610a5e9a2075ce9e6b59056be
lk <lasse.klein@gmail.com>
2022-09-14 21:44:24 +0200

    Globals + table name to env

--- functions/feedbackproxy.js
+++ functions/feedbackproxy.js
@@ -1,23 +1,14 @@
 require('dotenv').config();
 const Airtable = require('airtable')
+Airtable.configure({apiKey: process.env.AIRTABLE_API_KEY})
+const base  = Airtable.base(process.env.AIRTABLE_BASE_ID)
+const table = base(process.env.AIRTABLE_TABLE_NAME)
 
 exports.handler = async (event) => {
-    Airtable.configure({apiKey: process.env.AIRTABLE_API_KEY})
-    const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
-    const baseName = 'FeedbackForm'
     if (event['httpMethod'] === 'POST') {
-        let post = JSON.parse(event.body)
-        const data = [{'fields': post}]
-
-        await base(baseName).create(data, {typecast: true} )
-
-        return {
-            statusCode: 200,
-            body: 'Success!',
-        }
+        const data = [{'fields': JSON.parse(event.body)}]
+        await table.create(data, {typecast: true} )
+        return { statusCode: 200, body: 'Success!' }
     }
-    return {
-        statusCode: 500,
-        body: 'Nay!',
-    }
+    return { statusCode: 500, body: 'Nay!' }
 }

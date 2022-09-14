2c1aa49d8c025a1cce290be4ff99e275069686d3
lk <lasse.klein@gmail.com>
2022-09-14 19:18:48 +0200

    Start

---
+++ functions/feedbackproxy.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const qs = require('querystring');
+const Airtable = require('airtable')
+
+exports.handler = async (event, context) => {
+    Airtable.configure({apiKey: process.env.AIRTABLE_API_KEY})
+    const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
+    const baseName = 'FeedbackForm'
+    if (event['httpMethod'] == 'POST') {
+        let post = JSON.parse(event.body)
+        const data = [{'fields': post}]
+
+        await base(baseName).create(data,
+            {typecast: true},
+            function (err,records) {
+                if (err) {console.error(err); return; }
+                records.forEach(function (record) {
+                    console.log(record.getId())
+                })
+            }
+        )
+        return {
+            statusCode: 200,
+            body: 'Success!',
+        }
+    }
+    return {
+        statusCode: 500,
+        body: 'Nay!',
+    }
+}
